ef6b3ec5c1def56b9b2642bc549d28faa9dfb3c4
Doğukaan Kılıçarslan <40922278+darkbringer1@users.noreply.github.com>
2022-07-27 15:03:54 +0300

    Remove HTTPHeaderType from returnUrlRequest

--- Sources/DefaultNetworkOperationPackage/APILayers/ManagerModules/APIServiceProvider.swift
+++ Sources/DefaultNetworkOperationPackage/APILayers/ManagerModules/APIServiceProvider.swift
@@ -25,7 +25,7 @@ open class ApiServiceProvider<T: Codable>: URLRequestProtocol {
         self.data = data
     }
     
-    public func returnUrlRequest() throws -> URLRequest {
+    public func returnUrlRequest(headerType header: HTTPHeaderFields) throws -> URLRequest {
         
         var url = try baseUrl.asURL()
         
@@ -35,7 +35,7 @@ open class ApiServiceProvider<T: Codable>: URLRequestProtocol {
         
         var request = URLRequest(url: url)
         request.httpMethod = method.rawValue
-        request.headers = getHeaders(header: header)
+//        request.headers = getHeaders(header: header)
         
         try configureEncoding(request: &request)
         
@@ -57,11 +57,11 @@ open class ApiServiceProvider<T: Codable>: URLRequestProtocol {
         return data.asDictionary()
     }
     
-    private func getHeaders(header: HTTPHeaderFields) -> HTTPHeaders {
-        var httpHeaders = HTTPHeaders()
-        httpHeaders.add(HTTPHeader(name: header.value.0, value: header.value.1))
-        return httpHeaders
-    }
+//    private func getHeaders(header: HTTPHeaderFields) -> HTTPHeaders {
+//        var httpHeaders = HTTPHeaders()
+//        httpHeaders.add(HTTPHeader(name: header.value.0, value: header.value.1))
+//        return httpHeaders
+//    }
     
     
 }
